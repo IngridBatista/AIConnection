d2ae3eae6d3e7e7e7054caf78c79da497f085e2e
fingrid <fingridbatista@gmail.com>
2026-01-24 11:20:05 -0300

    Melhoria na lógica de ClassGenerationService

--- AIConnection/Services/ClassGeneration/ClassGenerationService.cs
+++ AIConnection/Services/ClassGeneration/ClassGenerationService.cs
@@ -10,13 +10,42 @@ namespace AIConnection.Services.ClassGeneration
 {
     public static class ClassGenerationService
     {
-        private const string CODIGO_COMPLETO = "Código completo";
+        private const string CODIGO_COMPLETO = "CÓDIGO COMPLETO";
+        private const string IMPLEMETACAO_COMPLETA = "IMPLEMENTAÇÃO COMPLETA";
+        private const string SOLUCAO_RECOMENDADA = "SOLUÇÃO RECOMENDADA";
+        private const string RECOMENDADO = "RECOMENDADO";
+        private const string MAIS_CONCISA = "MAIS CONCISA";
+
+        private static readonly string[] completeSolutionKey = [
+            CODIGO_COMPLETO,
+            IMPLEMETACAO_COMPLETA
+        ];
+
+        private static readonly string[] recommendedSolutionKey = [
+            SOLUCAO_RECOMENDADA,
+            RECOMENDADO,
+            MAIS_CONCISA
+        ];
+
+        private static readonly string[] keys = [
+            "PASSO A PASSO",
+            "IMPLEMENTAÇÃO EM C#",
+            "SOLUÇÃO COMPLETA",
+            "IMPLEMENTAÇÃO COMPLETA",
+            "CÓDIGO FINAL COMPLETO",
+            "DIVIDIDA",
+            "SOLUÇÃO EM C#",
+            "SOLUÇÃO"
+        ];
 
         public static void CreateClassFile(LargeLanguageModelRequest llmRequest, string llmResponse)
         {
-            if (llmResponse.Contains(CODIGO_COMPLETO))
+            string? recommendedSolutionKeyFound = recommendedSolutionKey.FirstOrDefault(key => llmResponse.Contains(key, StringComparison.OrdinalIgnoreCase));
+            string? completeSolutionKeyFound = completeSolutionKey.FirstOrDefault(key => llmResponse.Contains(key, StringComparison.OrdinalIgnoreCase));
+
+            if (completeSolutionKeyFound != null)
             {
-                int index = llmResponse.IndexOf(CODIGO_COMPLETO);
+                int index = llmResponse.IndexOf(completeSolutionKeyFound);
 
                 llmResponse = index >= 0 ? llmResponse[(index)..].Trim() : llmResponse;
 
@@ -27,6 +56,72 @@ namespace AIConnection.Services.ClassGeneration
                     (string code, string className) result = NormalizeClass(llmRequest, match);
 
                     CreateFile(llmRequest, result.className, result.code);
+                }
+            }
+            else if (recommendedSolutionKeyFound != null)
+            {
+                int index = llmResponse.IndexOf(recommendedSolutionKeyFound);
+
+                llmResponse = index >= 0 ? llmResponse[(index)..].Trim() : llmResponse;
+
+                var match = Regex.Match(llmResponse, @"```csharp([\s\S]*?)```", RegexOptions.IgnoreCase);
+
+                (string code, string className) result = NormalizeClass(llmRequest, match);
+
+                CreateFile(llmRequest, result.className, result.code);
+            }
+            else if (llmResponse.Any(key => llmResponse.ToUpper().Contains(key)) && llmResponse.Contains("#"))
+            {
+                bool contemChave = keys.Any(key => llmResponse.IndexOf(key, StringComparison.OrdinalIgnoreCase) >= 0);
+
+                if (contemChave)
+                {
+                    var wordList = llmResponse
+                   .Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Where(l =>
+                        Regex.IsMatch(
+                            l,
+                            @"^#{1,3}\s+.*(\b(classe|c#|\.cs|implementação)\b|\d)",
+                            RegexOptions.IgnoreCase
+                        )
+                    ).Select(l => l.Trim()).ToList();
+
+                    int successfulMatchCount = 0; 
+
+                    foreach (var item in wordList)
+                    {
+                        if (!item.ToUpper().Contains("ANÁLISE") && !item.ToUpper().Contains("EXEMPLO")
+                            && !item.ToUpper().Contains("COMPILAR") && !item.ToUpper().Contains("COMPILAÇÃO")
+                            && !item.ToUpper().Contains("EXECUTAR") && !item.ToUpper().Contains("EXECUÇÃO")
+                            && !item.ToUpper().Contains("VERIFICAÇÃO"))
+                        {
+                            int index = llmResponse.IndexOf(item);
+
+                            var newLlmResponse = index >= 0 ? llmResponse[(index)..].Trim() : llmResponse;
+
+                            var match = Regex.Match(newLlmResponse, @"```csharp([\s\S]*?)```", RegexOptions.IgnoreCase);
+
+                            if (!match.Success || newLlmResponse.ToUpper().Contains("EXEMPLO"))
+                            {
+                                continue;
+                            }
+
+                            (string code, string className) result = NormalizeClass(llmRequest, match);
+
+                            CreateFile(llmRequest, result.className, result.code);
+
+                            successfulMatchCount++;
+                        }
+                    }
+
+                    if (successfulMatchCount == 0)
+                    {
+                        var match = Regex.Match(llmResponse, @"```csharp([\s\S]*?)```", RegexOptions.IgnoreCase);
+
+                        (string code, string className) result = NormalizeClass(llmRequest, match);
+
+                        CreateFile(llmRequest, result.className, result.code);
+                    }
                 }
             }
             else
